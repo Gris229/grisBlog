c3b80086e456d608b87f163f2aa43654a0c665d3
grid3v <gehunelgbedji@gmail.com>
2026-05-24 15:20:45 +0200

    upload database n PDO connexion files

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+try
+{
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=blogetu;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+//CECI EST UN TEST POUR S'ASSURER QUE LA LIAISON AVEC LA BD FONCTIONNE SANS AUCUN PROBLEME
+
+// On récupère tout le contenu de la table recipes
+$sqlQuery = 'SELECT * FROM users';
+$usersStatement = $mysqlClient->prepare($sqlQuery);
+$usersStatement->execute();
+$users = $usersStatement->fetchAll();
+
+// On affiche chaque recette une à une
+foreach ($users as $user) {
+?>
+    <p><?php echo $user['username'].' h '.$user['email']; ?></p>
+<?php
+}
+?>
